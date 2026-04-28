800a692e3592ce72b17601e89c5efb779037c534
Jason Elison <jason.elison@gmail.com>
2026-04-27 23:36:38 -0400

    Continue working on parser (forgot to stage schemas)

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import NamedTuple
+from parser import Condition, Command, Update, Action, Term, Expression
 
 class Token(NamedTuple):
     tokenType: str|None
@@ -10,6 +11,23 @@ class Token(NamedTuple):
 class TokenLexeme(NamedTuple):
     tokenType: str|None
     lexeme: str
+
+class ConditionCommandTuple(NamedTuple):
+    condition: Condition
+    command: Command
+
+class CommandTuple(NamedTuple):
+    update: list[Update]
+    action: Action
+
+class RelationTuple(NamedTuple):
+    leftExpression: Expression
+    rightExpression: Expression
+    relationalOperator: TokenLexeme
+
+class TermTuple(NamedTuple):
+    term: Term
+    addOp: TokenLexeme
 
 class CritterParseError(Exception):
     pass
