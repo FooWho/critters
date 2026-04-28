acdfbe159661b22a560d8192f3f82c13bc9435a4
Jason Elison <jason.elison@gmail.com>
2026-04-27 23:36:07 -0400

    Continue working on parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Optional, ClassVar, Iterable
+from typing import ClassVar, Iterable
+from schemas import ConditionCommandTuple, TokenLexeme, TermTuple, RelationTuple
 
 class ASTNode():
     _children: ClassVar[tuple[str, ...]] = ()
@@ -13,37 +14,57 @@ class ASTNode():
             elif isinstance(value, ASTNode):
                 yield value
 
-    
 class Program(ASTNode):
     _children: ClassVar[tuple[str]] = ('rules',)
 
-    def __init__(self, rules: list[Rule] = []) -> None:
-        self.rules: list[Rule] = rules
-
-    def addRule(self, rule: Rule|None = None, condition: Condition|None = None, command: Command|None = None) -> None:
-        if rule is not None and (condition is None and command is None):
-            self.rules.append(rule)
-        elif rule is None and (condition is not None and command is not None):
-            self.rules.append(Rule(condition=condition, command=command))
+    def __init__(self, rules: list[Rule]|None = None) -> None:
+        self.rules: list[Rule]
+        if rules is not None:
+            self.rules = rules
         else:
-            raise ValueError('Invalid arguments: Provide either a "rule" or both a "condition" and a "command".')
-        
+            self.rules = []
 
+    def addRule(self, rule: Rule|None = None, 
+                conditionCommandTuple: ConditionCommandTuple|None = None) -> None:
+        if rule is not None and conditionCommandTuple is None:
+            self.rules.append(rule)
+        elif rule is None and conditionCommandTuple is not None:
+            self.rules.append(Rule(conditionCommandTuple))
+        else:
+            raise ValueError('Invalid arguments: Provide either a "rule" or both a'
+                             ' "condition" and a "command".')
+        
 
 class Rule(ASTNode):
     _children: ClassVar[tuple[str, str]] = ('condition', 'command')
     
-    def __init__(self, condition: Optional[Condition] = None, command: Optional[Command] = None) -> None:
-        self.condition = condition
-        self.command = command
+    def __init__(self, conditionCommandTuple: ConditionCommandTuple|None = None) -> None:
+        self.condition: Condition
+        self.command: Command
+        if conditionCommandTuple is not None:
+            self.condition = conditionCommandTuple.condition
+            self.command = conditionCommandTuple.command
+        else:
+            self.condition = Condition()
+            self.command = Command() 
 
-    def setRule(self, condition: Condition, command: Command) -> None: 
-        self.condition = condition
-        self.command = command
+    def setRule(self, conditionCommandTuple: ConditionCommandTuple) -> None: 
+        self.condition = conditionCommandTuple.condition
+        self.command = conditionCommandTuple.command
 
 
 class Condition(ASTNode):
-    pass
+    _children: ClassVar[tuple[str]] = ('conjunctions',)
+    
+    def __init__(self, conjunctions: list[Conjunction]|None = None) -> None:
+        self.conjunctions: list[Conjunction]
+        if conjunctions is not None:
+            self.conjunctions = conjunctions
+        else:
+            self.conjunctions = []
+
+    def addConjunction(self, conjunction: Conjunction) -> None:
+        self.conjunctions.append(conjunction)
 
 
 class Command(ASTNode):
@@ -61,10 +82,46 @@ class Update(ASTNode):
 class Action(ASTNode):
     pass
 
+class Conjunction(ASTNode):
+    pass
+
 
 class Relation(ASTNode):
+    _children: ClassVar[tuple[str, str]] = ('leftExpression', 'rightExpression')
 
-    def __init__(self, root: Optional[Program] = None) -> None:
-        pass
+    def __init__(self, relationTuple: RelationTuple|None = None) -> None:
+        self.leftExpression: Expression|None = None
+        self.rightExpression: Expression|None = None
+        self.relationalOperator: TokenLexeme|None = None
+
+        if relationTuple is not None:
+            self.leftExpression = relationTuple.leftExpression
+            self.rightExpression = relationTuple.rightExpression
+            self.relationalOperator = relationTuple.relationalOperator
+
+
+
+
+
+
+class Expression(ASTNode):
+    _children: ClassVar[tuple[str]] = ('terms',)
+
+    def __init__(self, terms: list[Term]|None = None) -> None:
+        self.terms: list[Term] = []
+        if terms is not None:
+            self.terms = terms
+
+
+
+class Term(ASTNode):
+    _children: ClassVar[tuple[str, str]] = ('term', 'addop')
+
+    def __init__(self, term: TermTuple|None = None) -> None:
+        self.term: Term
+        self.addop: TokenLexeme
+        if term is not None:
+            self.term = term.term
+            self.addop = term.addOp
 
 
